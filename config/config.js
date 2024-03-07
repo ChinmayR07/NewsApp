0bd91961fbbb928e86be798a4c72ac5aad9ea0b8
ChinmayR07 <120701533+ChinmayR07@users.noreply.github.com>
2024-03-07 05:13:17 -0500

    Delete config/config.js

--- config/config.js
+++
@@ -1,4 +0,0 @@
-export const API_KEY = `02282069182944358aebcf7bd88f9908`;
-export const endpoint = `https://newsapi.org/v2/everything`;
-export const category = 'general';
-export const country = 'us'
