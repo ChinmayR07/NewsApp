9fc5c67bdde81629d5fe0986dfabb7ba74b15e96
ChinmayR07 <chinmayravindra.raichur@stonybrook.edu>
2024-03-07 05:10:42 -0500

    Bug fixes

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-export const API_KEY = `news-api-key`;
-export const endpoint = `https://newsapi.org/v2/top-headlines`;
+export const API_KEY = `02282069182944358aebcf7bd88f9908`;
+export const endpoint = `https://newsapi.org/v2/everything`;
 export const category = 'general';
 export const country = 'us'
